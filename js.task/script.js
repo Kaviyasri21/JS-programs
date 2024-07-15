c805f64fdd04c5772401a8646095575ff62bbe26
KAVIYASRI J <166154786+Kaviyasri21@users.noreply.github.com>
2024-07-15 16:35:33 +0530

    Add files via upload

---
+++ js.task/script.js
@@ -0,0 +1,22 @@
+function number(n)
+{
+    for(let i=0;i<=n; i++)
+    {
+        if( i%3==0 && i%5==0)
+        {
+            console.log("fizzBUZZ");
+        }
+        else if(i%3==0)
+        {
+            console.log("fizz");
+        }
+        else if(i%5==0)
+        {
+            console.log("buzz");
+        }
+        else{
+            console.log(i);
+        }
+    }
+}
+number(100);
